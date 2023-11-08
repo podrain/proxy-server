671709739d97ece46a6488c93b127c3546102931
Joe Sweeney <joe.sweeney224@gmail.com>
2023-11-08 12:17:50 -0700

    try adding cors

--- app.ts
+++ app.ts
@@ -1,19 +1,13 @@
-import { decode } from "https://deno.land/std@0.106.0/encoding/base64.ts"
 import { serve } from "https://deno.land/std@0.184.0/http/server.ts"
 import { Hono } from "https://deno.land/x/hono@v3.9.0/mod.ts"
 import { cors } from 'https://deno.land/x/hono@v3.9.0/middleware.ts'
+import "https://deno.land/std@0.205.0/dotenv/load.ts";
 
 const app = new Hono()
 
-// app.use(cors({
-//   origin: 'http://localhost:8000',
-//   allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
-//   allowMethods: ['POST', 'GET', 'OPTIONS'],
-//   exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
-//   maxAge: 600,
-//   credentials: true,
-
-// }))
+app.use(cors({
+  origin: Deno.env.get('ALLOWED_DOMAINS')?.split(',') || [],
+}))
 
 app.get('/:proxy_url', async (c) => {
   const raw_url = c.req.param('proxy_url')
